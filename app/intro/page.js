39412b0fa6c0893f87aab8ee18a23205deb8a315
uk030621 <123797901+uk030621@users.noreply.github.com>
2024-11-10 22:03:54 +0000

    Added crypto to intro list

--- app/intro/page.js
+++ app/intro/page.js
@@ -1,8 +1,8 @@
 "use client"; // This tells Next.js that this component should be rendered on the client side
 
-import { useState, useEffect } from 'react';
-import { useRouter } from 'next/navigation';
-import styles from './page.module.css'; // Import your CSS module
+import { useState, useEffect } from "react";
+import { useRouter } from "next/navigation";
+import styles from "./page.module.css"; // Import your CSS module
 
 const Introduction = () => {
   const [showIntroduction, setShowIntroduction] = useState(true);
@@ -11,9 +11,9 @@ const Introduction = () => {
 
   // Check localStorage to determine if the user has opted out
   useEffect(() => {
-    const hideIntro = localStorage.getItem('hideIntroduction');
-    if (hideIntro === 'true') {
-      router.push('/ukstock'); // Redirect immediately if the introduction is set to be hidden
+    const hideIntro = localStorage.getItem("hideIntroduction");
+    if (hideIntro === "true") {
+      router.push("/ukstock"); // Redirect immediately if the introduction is set to be hidden
     } else {
       setShowIntroduction(true); // Show the introduction if not opted out
     }
@@ -25,10 +25,10 @@ const Introduction = () => {
 
   const handleContinue = () => {
     if (hideIntroductionChecked) {
-      localStorage.setItem('hideIntroduction', 'true');
+      localStorage.setItem("hideIntroduction", "true");
     }
     setShowIntroduction(false);
-    router.push('/ukstock'); // Redirect to homepage or another default page
+    router.push("/ukstock"); // Redirect to homepage or another default page
   };
 
   if (!showIntroduction) {
@@ -41,22 +41,40 @@ const Introduction = () => {
       <p className={styles.description}>
         This app allows you to manage and monitor your stock portfolio easily.
         You can add, edit, and delete stocks from your portfolio, track their
-        performance, and see real-time changes in value. Here&apos;s what the app
-        offers:
+        performance, and see real-time changes in value. Here&apos;s what the
+        app offers:
       </p>
       <ul className={styles.list}>
-        <li className={styles.listItem}>Manage stocks from major indices like FTSE, Dow Jones, Nikkei, and DAX.</li>
-        <li className={styles.listItem}>Track stock symbols, prices, shares held, and total values per stock.</li>
-        <li className={styles.listItem}>Set a baseline value for your portfolio and monitor the indicative total portfolio value.</li>
-        <li className={styles.listItem}>View key statistics like value change from baseline and percentage change.</li>
-        <li className={styles.listItem}>Access support tools like Symbol Lookup and Currency Converter for a seamless experience.</li>
+        <li className={styles.listItem}>
+          Manage stocks from major indices like FTSE, Dow Jones, Nikkei, and
+          DAX.
+        </li>
+        <li className={styles.listItem}>
+          Track stock symbols, prices, shares held, and total values per stock.
+        </li>
+        <li className={styles.listItem}>
+          Set a baseline value for your portfolio and monitor the indicative
+          total portfolio value.
+        </li>
+        <li className={styles.listItem}>
+          View key statistics like value change from baseline and percentage
+          change.
+        </li>
+        <li className={styles.listItem}>
+          Access support tools like Symbol Lookup and Currency Converter for a
+          seamless experience.
+        </li>
+        <li className={styles.listItem}>
+          View current value and comprehensive details about all available
+          Cryptocurrencies.
+        </li>
       </ul>
       <div className={styles.checkboxContainer}>
         <label>
-          <input 
-            type="checkbox" 
-            checked={hideIntroductionChecked} 
-            onChange={handleCheckboxChange} 
+          <input
+            type="checkbox"
+            checked={hideIntroductionChecked}
+            onChange={handleCheckboxChange}
           />
           Do not show me this again
         </label>
